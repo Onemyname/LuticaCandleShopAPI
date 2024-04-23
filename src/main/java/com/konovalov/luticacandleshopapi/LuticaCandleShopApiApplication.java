002f6562ec9d49b7ad8cbe088747172f1bfb1715
Вадим Коновалов <vkonovalov@MacBook-Pro-Vadim.local>
2024-04-23 12:59:49 +0300

    INIT: init repo

---
+++ src/main/java/com/konovalov/luticacandleshopapi/LuticaCandleShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.konovalov.luticacandleshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuticaCandleShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuticaCandleShopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konovalov/luticacandleshopapi/LuticaCandleShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konovalov.luticacandleshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuticaCandleShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
